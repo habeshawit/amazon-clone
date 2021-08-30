39e2178dd68f4e59b413a1d7e18d3ffd1ed3cb02
habeshawit <shalomseleshi@gmail.com>
2021-08-30 02:07:01 -0700

    set up home and header components and css files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/"> 
+            <Header />  
+            <Home /> 
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
