804ca6351062c7cb90d59f1edc5b78f1825fe7d0
habeshawit <shalomseleshi@gmail.com>
2021-08-30 02:18:25 -0700

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./Home";
+import Header from "./Header";
 
 function App() {
   return (
