0166f5b555339afc55c2e01da7b57cd807e934f7
habeshawit <shalomseleshi@gmail.com>
2021-09-17 15:23:48 -0700

    initial set up firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./Home";
 import Login from "./Login";
 import Header from "./Header";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+
+// import firebase from 'firebase'
+// require('firebase/auth')
+import { useStateValue } from "./StateProvider";
 
 function App() {
+
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+  
+
+  
   return (
     <div className="App">
       <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,41 +1,34 @@
+import firebase from "firebase";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC6A9qzSjbmdffdOBNXL5n3FVYTwjQytak",
-  authDomain: "clone-65319.firebaseapp.com",
-  projectId: "clone-65319",
-  storageBucket: "clone-65319.appspot.com",
-  messagingSenderId: "235327162673",
-  appId: "1:235327162673:web:952f22768b302bd139c810",
-  measurementId: "G-16T7R52E3T"
+  apiKey: "AIzaSyCVwYXzBFZrpjV7UKQP_0TWEb96XHUfyEc",
+  authDomain: "amzn-clone-3b499.firebaseapp.com",
+  projectId: "amzn-clone-3b499",
+  storageBucket: "amzn-clone-3b499.appspot.com",
+  messagingSenderId: "874603285501",
+  appId: "1:874603285501:web:b2fa88181026d9ecaba794",
+  measurementId: "G-NQPQNS74PJ"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const db = getFirestore(app);
 
-const auth = getAuth();
-// createUserWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in 
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ..
-//   });
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
 
 export { db, auth };
